a3a9e71225a315a674a23d8e0bcfbd2f089d808b
pedsandom <pedsandom@alum.us.es>
2024-02-20 20:40:49 +0100

    Entidad objective

---
+++ src/main/java/acme/entities/objective/Objective.java
@@ -0,0 +1,50 @@
+
+package acme.entities.objective;
+
+import java.time.Duration;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class Objective {
+
+	@Past
+	@NotNull
+	private Date		instantiationMoment;
+
+	@NotBlank
+	@Length(max = 76)
+	@NotNull
+	private String		title;
+
+	@NotBlank
+	@Length(max = 101)
+	private String		description;
+
+	@NotNull
+	private Priority	priority;
+
+	@NotNull
+	private Boolean		status;
+
+	@NotNull
+	@FutureOrPresent
+	private Duration	duration;
+
+	@URL
+	private String		link;
+}
