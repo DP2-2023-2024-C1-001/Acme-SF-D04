b24254a262f3155eedd42369200e12c2e215c8b3
JOSE MARIA BAQUERO <josbaqrod@alum.us.es>
2024-03-02 19:15:53 +0100

    Corrección tarea individual 3 del estudiante 4

--- src/main/java/acme/entities/invoice/Invoice.java
+++ src/main/java/acme/entities/invoice/Invoice.java
@@ -4,9 +4,12 @@ package acme.entities.invoice;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -17,6 +20,7 @@ import javax.validation.constraints.Positive;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
+import acme.entities.sponsorship.Sponsorship;
 
 public class Invoice extends AbstractEntity {
 
@@ -47,6 +51,7 @@ public class Invoice extends AbstractEntity {
 	private double				quantity;
 
 	@Min(0)
+	@Max(100)
 	@NotNull
 	private double				tax;
 
@@ -67,5 +72,13 @@ public class Invoice extends AbstractEntity {
 
 		return result;
 	}
+
+	// Relationships ----------------------------------------------------------
+
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Sponsorship sponsorship;
 
 }
