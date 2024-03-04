2eb32c90569c8fcb454aa7698d4acba5ab9104f9
guillegomezz8 <guigomrom@alum.us.es>
2024-03-04 17:20:21 +0100

    Tarea 4 hecha

---
+++ src/main/java/acme/forms/ManagerDashBoard.java
@@ -0,0 +1,27 @@
+
+package acme.forms;
+
+import java.util.Map;
+
+import acme.client.data.AbstractForm;
+
+public class ManagerDashBoard extends AbstractForm {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	Map<Priority, Integer>		totalLecturesNumberByPriority;
+
+	Double						averageAmountOfUserStory;
+	Double						deviationAmountOfUserStory;
+	Double						minimumAmountOfUserStory;
+	Double						maximumAmountOfUserStory;
+
+	Double						averageAmountOfProject;
+	Double						deviationAmountOfProject;
+	Double						minimumAmountOfProject;
+	Double						maximumAmountOfProject;
+}
