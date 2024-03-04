6e95dddb0d0e104b5fdce5b01f813d55eee0d0e3
PedroPabloSantos <pedsandom@alum.us.es>
2024-03-04 21:29:04 +0100

    Tarea individual 2 realizada

---
+++ src/main/java/acme/entities/codeaudit/CodeAudit.java
@@ -0,0 +1,54 @@
+
+package acme.entities.codeaudit;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class CodeAudit extends AbstractEntity {
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	@NotBlank
+	private String				code;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				execution;
+
+	@NotBlank
+	private Type				type;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				correctiveActions;
+
+	@NotNull
+	private Mark				mark;
+
+	@URL
+	private String				link;
+
+}
